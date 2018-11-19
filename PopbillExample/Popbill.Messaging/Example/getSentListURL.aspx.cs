75918932208885c6d279c2f1c88c9214c840f640
kimhyunjin <khjzzm@gmail.com>
2018-11-19 16:39:36 +0900

    url api update

---
+++ PopbillExample/Popbill.Messaging/Example/getSentListURL.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+namespace Popbill.Message.Example
+{
+    public partial class getSentListURL : System.Web.UI.Page
+    {
+        public String code = null;
+        public String message = null;
+        public String url = null;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            /**
+            * 팝빌 문자 전송 내역 URL을 반환합니다.
+            * 반환된 URL은 보안정책에 따라 30초의 유효시간을 갖습니다.
+            */
+
+            // 팝빌회원 사업자번호, '-' 제외 10자리
+            String testCorpNum = "1234567890";
+
+            // 팝빌회원 아이디
+            String testUserID = "testkorea";
+
+            try
+            {
+                url = Global.messageService.GetSentListURL(testCorpNum, testUserID);
+            }
+            catch (PopbillException ex)
+            {
+                code = ex.code.ToString();
+                message = ex.Message;
+            }
+        }
+    }
+}
---
+++ PopbillExample/Popbill.Taxinvoice.Example.csharp/Example/getSealURL.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="getSealURL.aspx.cs" Inherits="Popbill.Taxinvoice.Example.getSealURL" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>팝빌 세금계산서 SDK ASP.NET Example</title>
+    <link href="../Example.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+		<div id="content">
+			<p class="heading1">Response</p>
+			<br/>
+			<fieldset class="fieldset1">
+				<legend>팝빌 인감 및 첨부문서 등록 URL 확인</legend>
+				<ul>
+					<% if (!String.IsNullOrEmpty(code)) { %>
+						<li>Response.code : <%=code %> </li>
+						<li>Response.message : <%= message %></li>
+					<% } else {	%>
+						<li>url : <%= url %></li>
+					<% } %>
+				</ul>
+			</fieldset>
+		 </div>
+	</body>
+</html>
