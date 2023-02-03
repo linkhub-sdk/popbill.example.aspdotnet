1e70b8ab5fea480fd17b2190f6cf690c03704479
박지헌 <jhpark@linkhubcorp.com>
2023-02-03 17:37:48 +0900

    주석 링크 변경

--- PopbillExample/Popbill.Messaging/Example/getMessagesRN.aspx.cs
+++ PopbillExample/Popbill.Messaging/Example/getMessagesRN.aspx.cs
@@ -24,7 +24,7 @@ namespace Popbill.Message.Example
         {
             /**
              * 파트너가 할당한 전송요청 번호를 통해 문자 전송상태 및 결과를 확인합니다.
-             * - https://developers.popbill.com/reference/sms/dotnet/api/info#GetMessageResultRN
+             * - https://developers.popbill.com/reference/sms/dotnet/api/info#GetMessagesRN
              */
 
             // 팝빌회원 사업자번호, '-' 제외 10자리
