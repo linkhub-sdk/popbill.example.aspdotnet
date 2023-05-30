93b37159188625d946d57f885b6c58cda2c5dfc9
AlBaneo93 <albaneo0724@gmail.com>
2023-05-30 17:03:41 +0900

    Message/CheckAutoDenyNumber 주석 오타 수정

--- PopbillExample/Popbill.Messaging/Example/checkAutoDenyNumber.aspx.cs
+++ PopbillExample/Popbill.Messaging/Example/checkAutoDenyNumber.aspx.cs
@@ -16,7 +16,7 @@ namespace Popbill.Message.Example
 
             /**
              * 팝빌회원에 등록된 080 수신거부 번호 정보를 확인합니다.
-             * - https://developers.popbill.com/reference/sms/dotnet/api/send#CheckAutoDenyNumber
+             * - https://developers.popbill.com/reference/sms/dotnet/api/info#CheckAutoDenyNumber
              */
 
             // 팝빌회원 사업자번호, '-' 제외 10자리
