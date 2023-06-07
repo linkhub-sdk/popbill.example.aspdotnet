cdb835de2319200c48283574bd1ca282cd1bcbce
AlBaneo93 <albaneo0724@gmail.com>
2023-06-07 13:30:04 +0900

    전자명세서 - Issue 함수 Emailsubject 위치 변경 업데이트

--- PopbillExample/Popbill.Statement/Example/issue.aspx.cs
+++ PopbillExample/Popbill.Statement/Example/issue.aspx.cs
@@ -27,10 +27,10 @@ namespace Popbill.Statement.Example
              */
 
             // 팝빌회원 사업자번호, '-' 제외 10자리
-            String testCorpNum = "1234567890";
+            String CorpNum = "1234567890";
 
             // 팝빌회원 아이디
-            String testUserID = "testkorea";
+            String UserID = "testkorea";
 
             // 명세서 종류 코드 - 121(거래명세서), 122(청구서), 123(견적서), 124(발주서), 125(입금표), 126(영수증)
             int itemCode = 121;
@@ -47,7 +47,7 @@ namespace Popbill.Statement.Example
             try
             {
                 Response response =
-                    Global.statementService.Issue(testCorpNum, itemCode, mgtKey, memo, testUserID, EmailSubject);
+                    Global.statementService.Issue(CorpNum, itemCode, mgtKey, memo, EmailSubject, UserID);
 
                 code = response.code.ToString();
                 message = response.message;
