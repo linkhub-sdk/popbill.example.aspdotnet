33c1535409997aad271f0595b762974636d8bf38
정우석 <wsj@linkhubcorp.com>
2025-01-04 17:57:50 +0900

    V24-11-07 수정

--- PopbillExample/Popbill.HomeTax.Cashbill/Global.asax.cs
+++ PopbillExample/Popbill.HomeTax.Cashbill/Global.asax.cs
@@ -2,7 +2,7 @@
 * 팝빌 홈택스 현금영수증 API .NET SDK ASP.NET Example
 * ASP.NET 연동 튜토리얼 안내 : https://developers.popbill.com/guide/htcashbill/dotnet/getting-started/tutorial?fwn=asp
 *
-* 업데이트 일자 : 2024-11-12
+* 업데이트 일자 : 2025-01-04
 * 연동기술지원 연락처 : 1600-9854
 * 연동기술지원 이메일 : code@linkhubcorp.com
 *         
